d38cadf03dbb0b28a77c3a3908e4f74c21932813
Chris <chriswang.csc@gmail.com>
2022-10-09 15:54:12 -0600

    Initial commit

---
+++ ad-eureka/src/main/java/pers/me/ad/EurekaApplication.java
@@ -0,0 +1,21 @@
+package pers.me.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Chris Wang
+ * @version 1.0
+ * @date 2022-10-08 9:46 p.m.
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
